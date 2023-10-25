4a44ea74bc4c580db95173834706ed76f9021245
Mawuli Hormeku <hormekumawuli93@gmail.com>
2023-10-25 16:32:50 +0000

    adding comments

--- assets/updating_entries.py
+++ assets/updating_entries.py
@@ -10,6 +10,7 @@ mycursor = mydb.cursor()
 # this is the update function
 # sql = "UPDATE students SET age = 19 WHERE name = 'Bob'"
 
+# this is for the limit command 
 mycursor.execute("SELECT * FROM students LIMIT 3 OFFSET 2")
 
 myresult = mycursor.fetchall()
